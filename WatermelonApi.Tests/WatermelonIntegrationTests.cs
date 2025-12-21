a7d8213424f4031e808b169c9831f9a631b43c3c
Ryu Martinez <ryueduardomartinez@outlook.es>
2025-12-20 21:07:31 -0300

    Pull_InitialSync_ReturnsAllData

--- WatermelonApi.Tests/WatermelonIntegrationTests.cs
+++ WatermelonApi.Tests/WatermelonIntegrationTests.cs
@@ -1,33 +1,25 @@
 ﻿using System.Net.Http.Json;
 using Microsoft.Extensions.DependencyInjection;
-using Xunit;
 
 namespace WatermelonApi.Tests;
 
-public class WatermelonIntegrationTests : IClassFixture<WatermelonApiFactory>
+public class WatermelonIntegrationTests(WatermelonApiFactory factory) : IClassFixture<WatermelonApiFactory>
 {
-    private readonly WatermelonApiFactory _factory;
-    private readonly HttpClient _client;
-
-    public WatermelonIntegrationTests(WatermelonApiFactory factory)
-    {
-        _factory = factory;
-        _client = factory.CreateClient();
-    }
+    private readonly HttpClient _client = factory.CreateClient();
 
     private async Task ResetAndSeedDatabase()
     {
-        using var scope = _factory.Services.CreateScope();
+        using var scope = factory.Services.CreateScope();
         var context = scope.ServiceProvider.GetRequiredService<AppDbContext>();
-
-        // Clean and recreate schema
+        
         await context.Database.EnsureDeletedAsync();
         await context.Database.EnsureCreatedAsync();
 
         context.Products.Add(new Product { 
             Id = "prod_1", 
             Name = "Initial Product", 
-            LastModified = 1000 
+            LastModified = 1000,
+            ServerCreatedAt = 1000 
         });
         await context.SaveChangesAsync();
     }
